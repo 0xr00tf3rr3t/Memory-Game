f24125a27c96489861d918ef30ea883f2fd37b8c
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-05-20 16:23:36 -0400

    Version 0.0.3
    feat: Adds timer. 
    feat: Adds animation when using choosing wrong cards.

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-
     let selectedCards = [];
     let cards = {
         one: null,
@@ -20,23 +19,67 @@ document.addEventListener("DOMContentLoaded", function (event) {
         sixteen: null
 
     };
-
-    ;
-
-    let gameState = "stop"; // Current State of game
+    let startingTime;
+    let now;
+    let timer;
+    let intervalHold;
+  
+    
+    let gameState = "STOP"; // Current State of game for controlling the logic
+    let playedPairs = 7;
     const gameBoard = document.getElementById("game"); //Gets the board from DOM
     const btnStart = document.getElementById("btnStart");
     let cards1 = document.querySelectorAll(".card");
-
-
-    btnStart.addEventListener('click', function () //Adds a Click Event for the whole Board
+    let timerText=document.getElementById('count');
+  
+    btnStart.addEventListener('click', function () //Adds a Click Event for the start buttong
         {
-            gameState = "Start" //Changes the gameState to Start
+            startingTime= Date.now();
+            
+            gameState = "START" //Changes the gameState to Start
+            
             randomCards(); //Puts the random cards into the Array
             console.log(cards);
+           
+
+           intervalHold= setInterval(function timerLogic(){
+            let seconds=0;
+            let minutes=0;
+            now = Date.now();
+            timer=now-startingTime;
+            seconds=Math.floor(timer/1000);
+            if(seconds<60)
+            {
+              timerText.innerText='0:'+seconds;
+              if (seconds<10)
+              {
+                timerText.innerText='0:0'+seconds;
+              }
+              else if (seconds < 60)
+              {
+                timerText.innerText='0:'+seconds;
+              }
+            
+            }
+            else{
+                minutes=Math.floor(timer/60000);
+                seconds=(Math.floor(timer/1000)-(60*minutes));
+                timerText.innerText=minutes+':'+seconds;
+                if (seconds<10)
+                {
+                  timerText.innerText=minutes + ':0' + seconds;
+                }
+                else
+                {
+                  timerText.innerText=minutes+':'+seconds;
+                }
+            }
+
+           },1000)
         })
 
-    gameBoard.addEventListener('click', clicker, false)
+    gameBoard.addEventListener('click', clicker, false);
+
 
 
     function randomCards() { //Shuffles the cards
@@ -44,7 +87,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         if (cardsArray <= 16) {
             for (let i = 0; i < 16; i++) {
                 cardsArray.push(checkRepeated(cardsArray));
-
             };
         } else {
             cardsArray.length = 0;
@@ -68,31 +110,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
         cards.fourteen = cardsArray[13];
         cards.fifthteen = cardsArray[14];
         cards.sixteen = cardsArray[15];
+
     }
 
-
-
     function checkRepeated(cardsArray) { //Verifies that the number is only repeated  twice
-        var counter = 0;
+        let counter = 0;
         let number = 0;
-
-        number = getRandomInt(1, 9);//Gets the number to be used on  next position
-        console.log("Random Number: " + number)
-        for (const cardNumber of cardsArray) {//Iterates inside the array
-            if (parseInt(cardNumber) === number) {//convert number to int
+        number = getRandomInt(1, 9); //Gets the number to be used on  next position
+        //.log("Random Number: " + number)
+        for (const cardNumber of cardsArray) { //Iterates inside the array
+            if (parseInt(cardNumber) === number) { //convert number to int
                 counter++;
-                console.log("SAME NUMBER: " + cardNumber + " | " + number + " COUNT: " + counter);
+      //          console.log("SAME NUMBER: " + cardNumber + " | " + number + " COUNT: " + counter);
             }
         }
         if (counter < 2) {
-            return number;//if counter is less than 2 retunr number
-
+            return number; //if counter is less than 2 retunr number
         } else {
-            console.log("Repeating function with number : " + number)
-            number = checkRepeated(cardsArray);//Recursive function into itself again
+         //   console.log("Repeating function with number : " + number)
+            number = checkRepeated(cardsArray); //Recursive function into itself again
             return number;
         }
-
     }
 
     function getRandomInt(min, max) { //Gets Random Integer between two numbers
@@ -102,38 +140,64 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     function clicker(e) {
-        if (e.target !== e.currentTarget) {
-            let clickedItem = e.target.id;
-            let clicked = document.getElementById(clickedItem);
-            let clickedElements;
+        let clicked = document.getElementById(e.target.id);
+        let clickedElements;
 
-            if (gameState === "Start") {
-                
-                clickedElements=document.querySelectorAll(".selected");
-                if (clickedElements.length <1) {
-                    clicked.classList.add("selected");
+        if (gameState === "WAIT") {
+            clickedElements = document.querySelectorAll(".selected");
+            clickedElements.forEach(function (remover) {
+                remover.classList.remove('selected');
+                remover.classList.remove('incorrect');
+            })
+            gameState = "START";
+        }
+        if (e.target !== e.currentTarget) {
+
+
+            if (gameState === "START") { //Only verify if game have started
+
+                clickedElements = document.querySelectorAll(".selected"); //Gets all elements that have been clicked already
+                if (clickedElements.length < 1 && !clicked.classList.contains('correct')) { //If there is not any selected
+                    clicked.classList.add("selected"); //Only add a new class
                     console.log(clicked.classList);
+                } else if (clickedElements.length === 1 && !clicked.classList.contains('correct')) { //If there is one selected
+                    clicked.classList.add("selected"); //Add new class
+                    clickedElements = document.querySelectorAll(".selected"); //get all selected
+                    validateSelection(clickedElements); //Logic for what happens when two are selected
                 }
-            else if (clickedElements.length ===1)
-             {
-                clicked.classList.add("selected");
-                clickedElements=document.querySelectorAll(".selected");
-                validateSelection(clickedElements);
             }
         }
-        }
-        e.stopPropagation();
-       function validateSelection(clickedElements)
-       {
-        if( cards[clickedElements[0].id]=== cards[clickedElements[1].id])
-        {console.log("SAME");
-    }
-    else
-    {console.log("Wrong!")}
-       let remover=document.querySelector(".selected");
-       console.log(remover);
-       remover.classList.remove(".selected");
-       }
-    }
+        e.stopPropagation(); //Stop DOM from getting any higher than parent
 
+        function validateSelection(clickedElements) {
+            try {
+                if (cards[clickedElements[0].id] === cards[clickedElements[1].id]) { // If the two clicked elements are the same
+                    console.log("SAME");
+                    clickedElements.forEach(function (element) {
+                        element.classList.add('correct');
+                        element.classList.remove('selected');
+                    })
+
+                    playedPairs++; //How many have been guested goes up by one
+                    console.log(playedPairs);
+                    if (playedPairs === 8) {
+                        console.log("YOU WON!");
+                        gameState = "FINISHED";
+                        clearInterval(intervalHold);
+                        
+                    }
+
+                } else { // Else just remove selected 
+                    gameState = "WAIT";
+                clickedElements.forEach(function(element)
+                {
+                    element.classList.add('incorrect');
+                    setTimeout(function(){
+                        element.classList.remove('incorrect');
+                },500);
+                })
+                }
+            } catch (error) {}
+        }
+    }
 })
